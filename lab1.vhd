d4f443f37327b380e33b440d6fada42e55d6b5e3
Łukasz Niedźwiadek <37691734+SunBear1@users.noreply.github.com>
2022-03-05 13:52:18 +0100

    Add files via upload

---
+++ lab1.vhd
@@ -0,0 +1,46 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 03/05/2022 01:05:14 PM
+-- Design Name: 
+-- Module Name: main - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity main is
+    Port ( sw_i : in STD_LOGIC_VECTOR (7 downto 0);
+           led7_an_o : out STD_LOGIC_VECTOR (3 downto 0);
+           led7_seg_o : out STD_LOGIC_VECTOR (7 downto 0));
+end main;
+
+architecture Behavioral of main is
+
+begin
+    led7_seg_o <= "00100101";
+    led7_an_o <= "1110";
+
+end Behavioral;
