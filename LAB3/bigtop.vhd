12b66bce52e3672b04e8a35a4ec4643605d4357d
Łukasz Niedźwiadek <37691734+SunBear1@users.noreply.github.com>
2022-03-21 11:43:15 +0100

    Update bigtop.vhd
    nowy proces dla anod co cykl zegara, kropki kombinatorycznie prostym przypisaniem digits(0) = sw_i(4), resetowanie wszystkich segmentów oprócz kropek

--- LAB3/bigtop.vhd
+++ LAB3/bigtop.vhd
@@ -37,7 +37,7 @@ entity top is
 
     Port ( clk_i : in STD_LOGIC;
 
-           btn_i : in STD_LOGIC_VECTOR (4 downto 0);
+           btn_i : in STD_LOGIC_VECTOR (3 downto 0);
            
            rst_i : in STD_LOGIC;
            
@@ -55,37 +55,63 @@ signal hex_values : STD_LOGIC_VECTOR (3 downto 0) := "0000";
 signal dot_values : STD_LOGIC_VECTOR (3 downto 0) := "0000";
 signal led_digits : STD_LOGIC_VECTOR (6 downto 0) := "0000000";
 signal digits : STD_LOGIC_VECTOR (31 downto 0) := "00000000000000000000000000000000";
-signal active_an: std_logic_vector(1 downto 0) := "00";
-
+signal clk_div : STD_LOGIC  := '0';
 begin
 
-process(rst_i,clk_i)
-    constant N: integer := 100000; -- 100 MHz / 1 KHz
+process(clk_i)
     variable counter: integer := 0;
     begin
-    --zegar
-    if (rst_i = '1') then 
-        counter := 0;
-    elsif (rising_edge(clk_i)) then 
-        if(counter < N) then 
-            counter := counter + 1;
-        else
+    if (rising_edge(clk_i)) then
+        counter := counter + 1;
+        if counter = 50000 then
             counter := 0;
+            clk_div <= not clk_div;
+        end if;
+    end if;
+end process;
+
+
+
+process(rst_i,clk_div,btn_i,led_digits,sw_i,digits)
+    variable seg_setter: integer := 0;
+    begin
+        if clk_div = '1' then
+            seg_setter := seg_setter + 1;
+            
+            if seg_setter = 4 then
+                seg_setter := 0;
+            end if;
+            
             if btn_i(0) = '1' then 
-                digits(7 downto 1) <= led_digits;    
+                digits(7 downto 1) <= led_digits;
+                led7_seg_o <= digits(7 downto 0);    
             end if;
             if btn_i(1) = '1' then 
                 digits(15 downto 9) <= led_digits;
+                led7_seg_o <= digits(15 downto 8); 
             end if;
             if btn_i(2) = '1' then 
                 digits(23 downto 17) <= led_digits;
+                led7_seg_o <= digits(23 downto 16);
             end if;
             if btn_i(3) = '1' then 
                 digits(31 downto 25) <= led_digits;
+                led7_seg_o <= digits(31 downto 24);
             end if;
-            if btn_i(4) = '1' then 
+            if rst_i = '1' then 
                digits(31 downto 0) <= "00000011000000110000001100000011";
             end if;
+            
+            if seg_setter = 0 then
+                led7_an_o <= "0111";
+            elsif seg_setter = 1 then
+                led7_an_o <= "1011";
+            elsif seg_setter = 2 then
+                led7_an_o <= "1101";
+            elsif seg_setter = 3 then
+                led7_an_o <= "1110";
+            end if;
+                            
             if sw_i(4) = '1' then
                 digits(0) <= '0';
             end if;
@@ -97,17 +123,8 @@ process(rst_i,clk_i)
             end if;
             if sw_i(7) = '1' then
                 digits(24) <= '0';
-            end if;
-            --digits(0) <= '0' when sw_i(4) = '1' else 
---              '1';
---digits(8) <= '0' when sw_i(5) = '1' else 
---              '1';
-                --digits(16) <= '0' when sw_i(6) = '1' else 
---              '1';
-                --digits(24) <= '0' when sw_i(7) = '1' else 
---              '1';
-        end if;    
-      end if;
+            end if;  
+        end if;
 end process;
 
 hex_values <= sw_i(3 downto 0);
@@ -159,17 +176,19 @@ with hex_values select
           
 
 --zapalanie wyświetlaczy
-led7_seg_o <=  digits(31 downto 24) when (btn_i(3) = '1' and rst_i = '0') else
-						digits(23 downto 16) when (btn_i(2) = '1' and rst_i = '0') else
-						digits(15 downto 8) when (btn_i(1) = '1' and rst_i = '0') else
-						digits(7 downto 0) when (btn_i(0) = '1' and rst_i = '0') else
-						"11111111";
+            
+            --led7_seg_o <=  digits(31 downto 24) when (btn_i(3) = '1' and rst_i = '0') else
+					--	digits(23 downto 16) when (btn_i(2) = '1' and rst_i = '0') else
+					--	digits(15 downto 8) when (btn_i(1) = '1' and rst_i = '0') else
+					--	digits(7 downto 0) when (btn_i(0) = '1' and rst_i = '0');
+						--else
+						--"11111111";
 						
-led7_an_o <=   "0111" when (btn_i(3) = '1' and rst_i = '0') else 
-						"1011" when (btn_i(2) = '1' and rst_i = '0') else
-						"1101" when (btn_i(1) = '1' and rst_i = '0') else
-						"1110" when (btn_i(0) = '1' and rst_i = '0') else
-						"1111"; 
+--led7_an_o <=   "0111" when (btn_i(3) = '1' and rst_i = '0') else 
+--						"1011" when (btn_i(2) = '1' and rst_i = '0') else
+--						"1101" when (btn_i(1) = '1' and rst_i = '0') else
+--						"1110" when (btn_i(0) = '1' and rst_i = '0') else
+--						"1111"; 
 
 
 end Behavioral;
