ba5b00b00b80edcb2eab7131a5ee53c62a1da1c5
Łukasz Niedźwiadek <37691734+SunBear1@users.noreply.github.com>
2022-04-10 19:51:04 +0200

    Update display.vhd

--- LAB4/display.vhd
+++ LAB4/display.vhd
@@ -1,24 +1,3 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 03/23/2022 01:38:14 PM
--- Design Name: 
--- Module Name: display - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
-
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.STD_LOGIC_ARITH.all;
@@ -35,9 +14,7 @@ use IEEE.STD_LOGIC_UNSIGNED.all;
 
 entity display is
     Port ( clk_i : in STD_LOGIC;
-           
-           rst_i : in STD_LOGIC;
-           
+               
            digits_in : in STD_LOGIC_VECTOR (15 downto 0);
            
            led7_an_o : out STD_LOGIC_VECTOR (3 downto 0);
@@ -48,6 +25,7 @@ end display;
 architecture Behavioral of display is
 signal dot_values : STD_LOGIC_VECTOR (3 downto 0) := "0000";
 signal clk_div : STD_LOGIC  := '0';
+-- anoda_selector: std_logic_vector(1 downto 0) := "00";
 signal anoda_selector: std_logic_vector(1 downto 0) := "00";
 begin
 
@@ -58,18 +36,19 @@ process(clk_i)
         counter := counter + 1;
         if counter = 50000 then
             counter := 0;
-            --odúwieøanie wyúwietlacza
+            --od≈õwie≈ºanie wy≈õwietlacza
             anoda_selector <= anoda_selector + 1;
         end if;
     end if;
 end process;
 
 
-led7_seg_o <=  digits_in(15 downto 8) when (anoda_selector = "10" and rst_i = '0') else
-						digits_in(7 downto 0) when (anoda_selector = "11" and rst_i = '0') else
-						"00000000";
+led7_seg_o <= digits_in(15 downto 8) when (anoda_selector = "10") else
+						digits_in(7 downto 0) when (anoda_selector = "11");
 						
-led7_an_o <= "1101" when (anoda_selector = "10" and rst_i = '0') else
-                    "1110" when (anoda_selector = "11" and rst_i = '0') else
-                    "0000";
+led7_an_o <=   "0111" when (anoda_selector = "00") else 
+                    "1011" when (anoda_selector = "01") else
+                    "1101" when (anoda_selector = "10") else
+                    "1110" when (anoda_selector = "11");
+                    
 end Behavioral;
