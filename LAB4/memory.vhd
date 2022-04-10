a97ae03c997617a0379322568ae6fcff393e8960
Łukasz Niedźwiadek <37691734+SunBear1@users.noreply.github.com>
2022-04-10 19:51:22 +0200

    Update memory.vhd

--- LAB4/memory.vhd
+++ LAB4/memory.vhd
@@ -1,24 +1,3 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 03/23/2022 01:38:36 PM
--- Design Name: 
--- Module Name: memory - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
-
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.STD_LOGIC_ARITH.all;
@@ -35,7 +14,6 @@ use IEEE.STD_LOGIC_UNSIGNED.all;
 
 entity memory is
     Port ( clk_i : in STD_LOGIC;
-           rst_i : in STD_LOGIC;
            RXD_i : in STD_LOGIC;
            digits_out : out STD_LOGIC_VECTOR (15 downto 0));
 end memory;
@@ -46,73 +24,101 @@ signal led_code : STD_LOGIC_VECTOR (6 downto 0) := "0000000";
 signal clk_div : STD_LOGIC  := '0';
 signal digits : STD_LOGIC_VECTOR (15 downto 0) := "0000000000000000";
 signal bits_counter: integer := 0;
-signal begin_message: STD_LOGIC := '0';
-signal end_message: STD_LOGIC := '0';
 signal body_message: std_logic_vector(7 downto 0) := "00000000";
 
+signal clk_new_bit : STD_LOGIC  := '0';
+signal time_counter: integer := 0;
+signal transmission_started : STD_LOGIC  := '0';
+
 begin
-process(clk_i)
-    variable counter: integer := 0;
-    variable frequency: integer := 5208;
-    begin
-    if (rising_edge(clk_i)) then
-        -- u¿ywaj równoœci zamiast nierównoœci!!
-        counter := counter + 1;
-        
-        if counter = frequency then -- czy przerwano serie '1' ?
-            counter := 0;
-            bits_counter <= bits_counter + 1;
-            
-            if bits_counter = 1 then
-            
-                if RXD_i = '0' then
-                    begin_message <= '1';
-                end if;
-                
-            elsif bits_counter > 1 and bits_counter < 10 and begin_message = '1' then
-                body_message(bits_counter - 2) <= RXD_i;
-                
-            elsif bits_counter = 10 and begin_message = '1' then
-                end_message <= '1';
-                begin_message <= '0';
-                bits_counter <= 0;
-            end if;
-            
-            clk_div <= not clk_div;
-            
+
+-- check transmission state (started, stopped)
+process(RXD_i, bits_counter)
+variable counter: integer:= 0;
+    begin   -- czy tak moÅ¼na wykryÄ‡ zmiane z '1' na '0'?
+        if (RXD_i = '0' and bits_counter = 0) then 
+            -- wykrylismy poczatkowy bit nadawania sygnalu '0'
+            transmission_started <= '1';
+        elsif bits_counter = 9 then
+            transmission_started <= '0';                      
         end if;
-    end if;
 end process;
 
-process(clk_div, rst_i)
-variable bits_received: integer := 0;
+-- count time for one bit
+process(clk_i, time_counter)
+constant frequency: integer := 10416; 
     begin
-            if clk_div = '1' then
-                                                         
-            end if;
+        if bits_counter = 9 then
+                bits_counter <= 0;
+        end if;
+        if (transmission_started = '1') then
+                if (rising_edge(clk_i)) then
+                    if(time_counter = frequency/2) then
+                        bits_counter <= bits_counter + 1;
+                        time_counter <= time_counter + 1;
+                        clk_new_bit <= '1';
+                    elsif time_counter = frequency then
+                           clk_new_bit <= '0';
+                           time_counter <= 0;
+                    else
+                        time_counter <= time_counter + 1;
+                        clk_new_bit <= '0';   
+                    end if;                                   
+                end if;
+        end if;
+end process;
+    
+-- compose message from RXD_i signal
+process(RXD_i, bits_counter)
 
+variable counter: integer:= 0;
+    begin  -- mayby sequential ?
+        if (transmission_started = '1') then
+            if (clk_new_bit = '1' and bits_counter /=  9) then
+                body_message(bits_counter - 1) <= RXD_i;                                   
+            end if;
+        end if;
 end process;
 
-
-with hex_values select
-    led_code <= "0000001" when "0000",
-                "1001111" when "0001",
-                "0010010" when "0010",
-                "0000110" when "0011",
-                "1001100" when "0100",
-                "0100100" when "0101",
-                "0100000" when "0110",
-                "0001111" when "0111",
-                "0000000" when "1000",
-                "0000100" when "1001",
-                "0001000" when "1010",
-                "1100000" when "1011",
-                "0110001" when "1100",
-                "1000010" when "1101",
-                "0110000" when "1110",
-                "0111000" when "1111",
-                "0000000" when others;
+with body_message(3 downto 0) select
+    digits(7 downto 0) <= "00000011" when "0000",
+                "10011111" when "0001",
+                "00100101" when "0010",
+                "00001101" when "0011",
+                "10011001" when "0100",
+                "01001001" when "0101",
+                "01000001" when "0110",
+                "00011111" when "0111",
+                "00000001" when "1000",
+                "00001001" when "1001",
+                "00010001" when "1010",
+                "11000001" when "1011",
+                "01100011" when "1100",
+                "10000101" when "1101",
+                "01100001" when "1110",
+                "01110001" when "1111",
+                "00000001" when others;
                 
- digits_out <= digits;               
+with body_message(7 downto 4) select
+    digits(15 downto 8) <= "00000011" when "0000",
+                "10011111" when "0001",
+                "00100101" when "0010",
+                "00001101" when "0011",
+                "10011001" when "0100",
+                "01001001" when "0101",
+                "01000001" when "0110",
+                "00011111" when "0111",
+                "00000001" when "1000",
+                "00001001" when "1001",
+                "00010001" when "1010",
+                "11000001" when "1011",
+                "01100011" when "1100",
+                "10000101" when "1101",
+                "01100001" when "1110",
+                "01110001" when "1111",
+                "00000001" when others;
+                         
+ digits_out <= digits;   
+ 
 
 end Behavioral;
