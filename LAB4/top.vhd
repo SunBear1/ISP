d92385df9d35a5e6c9ecb5ec4f5355715b24507c
Łukasz Niedźwiadek <37691734+SunBear1@users.noreply.github.com>
2022-04-10 19:51:36 +0200

    Update top.vhd

--- LAB4/top.vhd
+++ LAB4/top.vhd
@@ -13,7 +13,6 @@ use IEEE.STD_LOGIC_1164.ALL;
 entity top is
 
     Port ( clk_i : in STD_LOGIC;
-           rst_i : in STD_LOGIC;
            RXD_i : in STD_LOGIC;
            led7_an_o : out STD_LOGIC_VECTOR (3 downto 0);
            led7_seg_o : out STD_LOGIC_VECTOR (7 downto 0));
@@ -22,13 +21,11 @@ end top;
 architecture Behavioral of top is
     component memory is
             Port ( clk_i : in STD_LOGIC;
-                   rst_i : in STD_LOGIC;
                    RXD_i : in STD_LOGIC;
                    digits_out : out STD_LOGIC_VECTOR (15 downto 0));
     end component memory;
     component display is
         Port ( clk_i : in STD_LOGIC;
-               rst_i : in STD_LOGIC;
                digits_in : in STD_LOGIC_VECTOR (15 downto 0);
                led7_an_o : out STD_LOGIC_VECTOR (3 downto 0);
                led7_seg_o : out STD_LOGIC_VECTOR (7 downto 0));
@@ -36,7 +33,7 @@ architecture Behavioral of top is
     signal digits : STD_LOGIC_VECTOR(15 downto 0) := (others => '0'); 
 begin
 
-memory_component: memory port map(clk_i => clk_i, digits_out => digits, rst_i => rst_i,RXD_i => RXD_i);
-display_component: display port map(clk_i => clk_i, digits_in => digits, led7_an_o => led7_an_o, led7_seg_o => led7_seg_o,rst_i => rst_i);
+memory_component: memory port map(clk_i => clk_i, digits_out => digits,RXD_i => RXD_i);
+display_component: display port map(clk_i => clk_i, digits_in => digits, led7_an_o => led7_an_o, led7_seg_o => led7_seg_o);
 
 end Behavioral;
